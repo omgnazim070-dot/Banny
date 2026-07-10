08f290c1a3b5a162748c957efb140c60796b3c86
omgnazim070-dot <omg.nazim.070@gmail.com>
2026-07-10 18:47:41 +0500

    Добавлен main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "[Banny]" << std::endl;
+    std::cout << "Запуск..." << std::endl;
+
+    std::cout << "Конфигурация загружена" << std::endl;
+    std::cout << "Логгер запущен" << std::endl;
+    std::cout << "Система готова" << std::endl;
+
+    return 0;
+}
